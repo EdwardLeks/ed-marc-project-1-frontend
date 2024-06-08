c3515ebd1afe26281962f735584d074cd2851969
Edward Leks <edwardleks@hotmail.com>
2024-06-08 17:32:50 +0300

    Started web page development

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,2 @@
 import 'normalize.css';
 import '../sass/style.sass';
-
-// --- To Delete ---
-const button = document.getElementById('button');
-button.addEventListener('click', function () {
-  document.getElementById('answer').innerHTML = `Thats great! :)`;
-  button.remove();
-});
-// --- To Delete ---
